4e2bfd548888cfc2533e928c99115fddffc3126a
ntomazin <nikola.tomazin@gmail.com>
2020-09-03 23:50:31 +0300

    Added new button for leaving markers Application initializes on current location

--- lib/main.dart
+++ lib/main.dart
@@ -1,128 +1,120 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 
-void main() {
-  runApp(MyApp());
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static LatLng _initialPosition = LatLng(45.521563, -122.677433);
+  final Set<Marker> _markers = {};
+
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Nap Alarm',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Nap Alarm'),
-    );
+  void initState(){
+    super.initState();
+    _getLocation();
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  Position _position;
+  /*
+    GeolocatorPlatform().getCurrentPosition().then((currloc){
+      setState((){
+        _initialPosition = currloc;0
+      });
+    });
+  }
+ */
 
   void _getLocation() async {
     Position position =
-        await getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    await getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
     setState(() {
-      _position = position;
+      _initialPosition = LatLng(position.latitude, position.longitude);
+    });
+  }
+
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  void _onMapTypeButtonPressed() {
+    setState(() {
+      _currentMapType = _currentMapType == MapType.normal
+          ? MapType.satellite
+          : MapType.normal;
+    });
+  }
+
+  MapType _currentMapType = MapType.normal;
+  LatLng _lastMapPosition = _initialPosition;
+
+  void _onCameraMove(CameraPosition position) {
+    _lastMapPosition = position.target;
+  }
+
+  void _onAddMarkerButtonPressed() {
+    setState(() {
+      _markers.add(Marker(
+        // This marker id can be anything that uniquely identifies each marker.
+        markerId: MarkerId(_lastMapPosition.toString()),
+        position: _lastMapPosition,
+        infoWindow: InfoWindow(
+          title: 'Really cool place',
+          snippet: '5 Star Rating',
+        ),
+        icon:   BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
+      ));
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Maps Sample App'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: Stack(
           children: <Widget>[
-            Text(
-              'Your location:',
-            ),
-            Text(
-              '$_position',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Stack(children: <Widget>[
-              Align(
-                alignment: Alignment.bottomLeft,
-                child: FloatingActionButton(
-                  heroTag: null,
-                  onPressed: _getLocation,
-                  tooltip: 'Get location',
-                  child: Icon(Icons.add_alarm),
-                ),
+            GoogleMap(
+              onMapCreated: _onMapCreated,
+              mapType: _currentMapType,
+              markers: _markers,
+              onCameraMove: _onCameraMove,
+              initialCameraPosition: CameraPosition(
+                target: _initialPosition,
+                zoom: 11.0,
               ),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: FloatingActionButton(
-                  heroTag: null,
-                  onPressed: _getLocation,
-                  tooltip: 'Get location',
-                  child: Icon(Icons.add_location),
-                ),
-              ),
-            ])
+            ),
+            Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Align(
+                  child: Column(children: <Widget>[
+                    FloatingActionButton(
+                      onPressed: _onMapTypeButtonPressed,
+                      materialTapTargetSize: MaterialTapTargetSize.padded,
+                      backgroundColor: Colors.green,
+                      child: const Icon(Icons.map, size: 36.0),
+                    ),
+                    SizedBox(height: 16.0),
+                    FloatingActionButton(
+                      onPressed: _onAddMarkerButtonPressed,
+                      materialTapTargetSize: MaterialTapTargetSize.padded,
+                      backgroundColor: Colors.green,
+                      child: const Icon(Icons.add_location, size: 36.0),
+                    ),
+                  ]),
+                )),
           ],
         ),
       ),
